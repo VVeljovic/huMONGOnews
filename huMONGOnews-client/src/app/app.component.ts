f0017b174d86a2e007cf530113a53a0636cbde54
VVeljovic <veljkoveljovic13@gmail.com>
2024-02-01 12:20:16 +0100

    psh

--- huMONGOnews-client/src/app/app.component.ts
+++ huMONGOnews-client/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { FrontPageComponent } from './front-page/front-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FrontPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
