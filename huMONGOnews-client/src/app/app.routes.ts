6351d89ad65ea469e4e3addc4c26b738f2d0427d
Mihajlo Madic <mihajlo.madic@elfak.rs>
2024-02-03 14:44:33 +0100

    Client app routes added

--- huMONGOnews-client/src/app/app.routes.ts
+++ huMONGOnews-client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CategoryPostsComponent } from './category-posts/category-posts.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'category-posts',
+    pathMatch: 'full',
+    component: CategoryPostsComponent,
+  },
+  { path: 'front-page', pathMatch: 'full', component: FrontPageComponent },
+  { path: 'header', pathMatch: 'full', component: HeaderComponent },
+];
